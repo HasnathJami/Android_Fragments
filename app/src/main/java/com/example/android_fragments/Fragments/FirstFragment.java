54c1ca23b951a663484592627b00d201318c24fe
jishan <jishanc46@gmail.com>
2022-02-14 18:54:15 +0600

    updated new changes

--- app/src/main/java/com/example/android_fragments/Fragments/FirstFragment.java
+++ app/src/main/java/com/example/android_fragments/Fragments/FirstFragment.java
@@ -1,5 +1,6 @@
 package com.example.android_fragments.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,16 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.android_fragments.R;
+import com.example.android_fragments.SecondActivity;
+
+import org.w3c.dom.Text;
 
 
 public class FirstFragment extends Fragment {
 
 
+    TextView tv;
+
     public FirstFragment() {
         // Required empty public constructor
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,6 +33,17 @@ public class FirstFragment extends Fragment {
         View view=inflater.inflate(R.layout.fragment_first, container, false);
 
         // we can do all tasks under this fragment here.
+        tv=view.findViewById(R.id.foTvId);
+
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent=new Intent(getContext(), SecondActivity.class);
+                startActivity(intent);
+
+            }
+        });
 
         return view;
     }
